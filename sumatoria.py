a7c368c8c3e4c39c50b0253d4cc94700cfa9ff5c
Bryan Diaz <bryandiaz13301@gmail.com>
2020-09-18 00:08:53 -0600

    Solución Tarea

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,17 @@
 def sumatoria_cubica(n):
+    suma=0
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            for k in range(j,i+j+1):
+                suma+=1
+    return suma
     raise NotImplementedError()
 
 def sumatoria_constante(n):
+    k= n*(n+1)
+    suma=((n+2)/3)*k
+    return suma
     raise NotImplementedError()
+
+
 
